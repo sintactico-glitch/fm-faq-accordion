5c936cc213b0aef84af18c69b4208eb8b4f0edc3
Paz Spera <mspera03@gmail.com>
2025-07-13 16:26:09 -0300

    chore: Deleted comments and console.log from js file

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-/* 
-1- get all icons by id
-2 - get all the faq-content
-
-3 - set up an event listener to match the different icons
-4a - flip the values of show and hide based on the icon click
-4b - change the src on the img of the icon
-
-*/
-
 let $faqIcon1 = document.querySelector("#faq-icon-1");
 let $faqIcon2 = document.querySelector("#faq-icon-2");
 let $faqIcon3 = document.querySelector("#faq-icon-3");
@@ -25,11 +15,9 @@ const $faqAnswer4 = document.querySelector("#faq-content-4");
 
 document.addEventListener("click", (e)=> {
   if(e.target === $faqIcon1 || e.target === $faqHeader1) {
-    console.log("icon 1");
     $faqAnswer1.hidden = !$faqAnswer1.hidden;
 
     if(!$faqAnswer1.hidden) {
-      console.log("minus icon should be shown")
       $faqIcon1.src = "./assets/images/icon-minus.svg"; 
     } else {
       $faqIcon1.src = "./assets/images/icon-plus.svg";
@@ -37,11 +25,9 @@ document.addEventListener("click", (e)=> {
   }
 
   if(e.target === $faqIcon2 || e.target === $faqHeader2) {
-    console.log("icon 2")
     $faqAnswer2.hidden = !$faqAnswer2.hidden;
 
     if(!$faqAnswer2.hidden) {
-      console.log("minus icon should be shown")
       $faqIcon2.src = "./assets/images/icon-minus.svg"; 
     } else {
       $faqIcon2.src = "./assets/images/icon-plus.svg";
@@ -49,11 +35,9 @@ document.addEventListener("click", (e)=> {
   }
 
   if(e.target === $faqIcon3 || e.target === $faqHeader3) {
-    console.log("icon 3")
     $faqAnswer3.hidden = !$faqAnswer3.hidden;
 
     if(!$faqAnswer3.hidden) {
-      console.log("minus icon should be shown")
       $faqIcon3.src = "./assets/images/icon-minus.svg"; 
     } else {
       $faqIcon3.src = "./assets/images/icon-plus.svg";
@@ -61,11 +45,9 @@ document.addEventListener("click", (e)=> {
   }
 
   if(e.target === $faqIcon4 || e.target === $faqHeader4) {
-    console.log("icon 4")
     $faqAnswer4.hidden = !$faqAnswer4.hidden;
 
     if(!$faqAnswer4.hidden) {
-      console.log("minus icon should be shown")
       $faqIcon4.src = "./assets/images/icon-minus.svg"; 
     } else {
       $faqIcon4.src = "./assets/images/icon-plus.svg";
